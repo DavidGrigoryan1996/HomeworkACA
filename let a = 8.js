9ddd67fae22e5c2700d9d66caaa80be48dbde247
DavidGrigoryan1996 <84630442+DavidGrigoryan1996@users.noreply.github.com>
2021-05-24 18:13:09 +0400

    Add files via upload

---
+++ let a = 8.js
@@ -0,0 +1,38 @@
+let a = 8
+undefined
+let b =12
+undefined
+a
+8
+b
+12
+a= a+b
+20
+b= a-b
+8
+a=a-b
+12
+a
+12
+b
+8
+
+
+
+
+let a= 10
+undefined
+let b =20
+undefined
+a
+10
+b
+20
+a=a+b
+30
+b=a-b
+10
+a=a-b
+20
+a
+20
